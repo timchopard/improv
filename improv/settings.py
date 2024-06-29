cd03d4014ee24c64fe1278f0069ced88419a1ef9
tim <timothyjchandler@gmail.com>
2024-06-29 13:06:52 +0200

    Minor updates to fix static files

--- improv/settings.py
+++ improv/settings.py
@@ -96,7 +96,8 @@ USE_TZ = True
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = PROJECT_ROOT
 print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
